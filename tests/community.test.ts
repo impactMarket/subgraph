f7e1f01946829e4b37c1f59fcf3e55161cc75e99
Bernardo Vieira <bernardo@impactmarket.com>
2021-12-17 15:49:11 +0000

    replace with parseEther

--- tests/community.test.ts
+++ tests/community.test.ts
@@ -1,3 +1,4 @@
+import { parseEther } from '@ethersproject/units';
 import { clearStore, test, assert } from 'matchstick-as/assembly/index';
 
 import { handleCommunityAdded } from '../src/mappings/communityAdmin';
@@ -9,13 +10,13 @@ test('create community', () => {
     const community = createCommunityAddedEvent(
         '0x1cad798788568098e51c5751fe03a8daa0c7eac6',
         ['0x7110b4df915cb92f53bc01cc9ab15f51e5dbb52f'],
-        '5',
-        '0',
-        '0',
-        '0',
-        '0',
-        '0',
-        '0'
+        parseEther('5').toString(),
+        parseEther('500').toString(),
+        parseEther('0.01').toString(),
+        '60',
+        '17280',
+        parseEther('1').toString(),
+        parseEther('50').toString()
     );
 
     handleCommunityAdded(community);
@@ -24,7 +25,7 @@ test('create community', () => {
         'CommunityEntity',
         '0x1cad798788568098e51c5751fe03a8daa0c7eac6',
         'claimAmount',
-        '5'
+        parseEther('5').toString()
     );
 
     clearStore();
