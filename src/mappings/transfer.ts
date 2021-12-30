3cdc813dc70b60143ed67f2b70a31c02cb5b1d2f
Bernardo Vieira <bernardo@impactmarket.com>
2021-12-30 11:19:04 +0000

    contributions

---
+++ src/mappings/transfer.ts
@@ -0,0 +1,49 @@
+import { Address } from '@graphprotocol/graph-ts';
+
+import { Transfer } from '../../generated/CeloDollar/CeloDollar';
+import { CommunityEntity, UBIEntity } from '../../generated/schema';
+import { treasuryAddress } from '../common/addresses';
+import { loadOrCreateCommunityDaily } from '../common/community';
+import { loadOrCreateDailyUbi } from '../common/ubi';
+
+export function handleTransferCeloDollar(event: Transfer): void {
+    const community = CommunityEntity.load(event.params.to.toHex());
+    if (community) {
+        const communityDaily = loadOrCreateCommunityDaily(
+            event.params.to,
+            event.block.timestamp
+        );
+        if (event.params.from.notEqual(Address.fromString(treasuryAddress))) {
+            // update ubi
+            const ubi = UBIEntity.load('0')!;
+            ubi.contributed = ubi.contributed.plus(event.params.amount);
+            ubi.save();
+            // update daily ubi
+            const ubiDaily = loadOrCreateDailyUbi(event.block.timestamp);
+            ubiDaily.contributed = ubiDaily.contributed.plus(
+                event.params.amount
+            );
+            ubiDaily.save();
+        }
+        // update community
+        community.totalContributed = community.totalContributed.plus(
+            event.params.amount
+        );
+        community.save();
+        // update community daily
+        communityDaily.contributed = communityDaily.contributed.plus(
+            event.params.amount
+        );
+        communityDaily.save();
+    } else if (event.params.to.equals(Address.fromString(treasuryAddress))) {
+        // update ubi
+        const ubi = UBIEntity.load('0')!;
+        ubi.contributed = ubi.contributed.plus(event.params.amount);
+        ubi.save();
+        // update daily ubi
+        const ubiDaily = loadOrCreateDailyUbi(event.block.timestamp);
+        ubiDaily.contributed = ubiDaily.contributed.plus(event.params.amount);
+        ubiDaily.save();
+    }
+    // nothing goes here!
+}
---
+++ tests/utils/transfer.ts
@@ -0,0 +1,31 @@
+import { Address, ethereum, BigInt } from '@graphprotocol/graph-ts';
+import { newMockEvent } from 'matchstick-as/assembly/defaults';
+
+import { Transfer } from '../../generated/CeloDollar/CeloDollar';
+
+export function createTransferEvent(
+    from: string,
+    to: string,
+    amount: string
+): Transfer {
+    const transferEvent = changetype<Transfer>(newMockEvent());
+    transferEvent.parameters = new Array();
+    const fromParam = new ethereum.EventParam(
+        'from',
+        ethereum.Value.fromAddress(Address.fromString(from))
+    );
+    const toParam = new ethereum.EventParam(
+        'to',
+        ethereum.Value.fromAddress(Address.fromString(to))
+    );
+    const amountParam = new ethereum.EventParam(
+        'amount',
+        ethereum.Value.fromUnsignedBigInt(BigInt.fromString(amount))
+    );
+
+    transferEvent.parameters.push(fromParam);
+    transferEvent.parameters.push(toParam);
+    transferEvent.parameters.push(amountParam);
+
+    return transferEvent;
+}
