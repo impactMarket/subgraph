dfcbca434e92739152c20df3353e4cb65858bae6
Bernardo Vieira <bernardo@impactmarket.com>
2022-01-03 17:51:34 +0000

    test

--- src/mappings/transfer.ts
+++ src/mappings/transfer.ts
@@ -8,42 +8,5 @@ import { loadOrCreateDailyUbi } from '../common/ubi';
 
 export function handleTransferCeloDollar(event: Transfer): void {
     const community = CommunityEntity.load(event.params.to.toHex());
-    if (community) {
-        const communityDaily = loadOrCreateCommunityDaily(
-            event.params.to,
-            event.block.timestamp
-        );
-        if (event.params.from.notEqual(Address.fromString(treasuryAddress))) {
-            // update ubi
-            const ubi = UBIEntity.load('0')!;
-            ubi.contributed = ubi.contributed.plus(event.params.amount);
-            ubi.save();
-            // update daily ubi
-            const ubiDaily = loadOrCreateDailyUbi(event.block.timestamp);
-            ubiDaily.contributed = ubiDaily.contributed.plus(
-                event.params.amount
-            );
-            ubiDaily.save();
-        }
-        // update community
-        community.totalContributed = community.totalContributed.plus(
-            event.params.amount
-        );
-        community.save();
-        // update community daily
-        communityDaily.contributed = communityDaily.contributed.plus(
-            event.params.amount
-        );
-        communityDaily.save();
-    } else if (event.params.to.equals(Address.fromString(treasuryAddress))) {
-        // update ubi
-        const ubi = UBIEntity.load('0')!;
-        ubi.contributed = ubi.contributed.plus(event.params.amount);
-        ubi.save();
-        // update daily ubi
-        const ubiDaily = loadOrCreateDailyUbi(event.block.timestamp);
-        ubiDaily.contributed = ubiDaily.contributed.plus(event.params.amount);
-        ubiDaily.save();
-    }
     // nothing goes here!
 }
