6458dfdb851e9d5f58555218f9b2619c5f8cd2de
Bernardo Vieira <bernardo@impactmarket.com>
2022-01-26 21:40:28 +0000

    add test

--- tests/manager.test.ts
+++ tests/manager.test.ts
@@ -115,5 +115,61 @@ test('remove manager', () => {
     clearStore();
 });
 
-// TODO: remove and readd manager
+test('remove manager', () => {
+    const community = createCommunityAddedEvent(
+        communityAddress[0],
+        [managerAddress[0]],
+        communityProps[0]
+    );
+
+    handleCommunityAdded(community);
+
+    const managerAddedEvent1 = createManagerAddedEvent(
+        managerAddress[0],
+        managerAddress[1],
+        communityAddress[0]
+    );
+
+    const managerAddedEvent2 = createManagerAddedEvent(
+        managerAddress[0],
+        managerAddress[2],
+        communityAddress[0]
+    );
+
+    handleManagerAdded(managerAddedEvent1);
+    handleManagerAdded(managerAddedEvent2);
+
+    const managerRemovedEvent1 = createManagerRemovedEvent(
+        managerAddress[0],
+        managerAddress[1],
+        communityAddress[0]
+    );
+
+    handleManagerRemoved(managerRemovedEvent1);
+
+    const managerAddedEvent3 = createManagerAddedEvent(
+        managerAddress[0],
+        managerAddress[1],
+        communityAddress[0]
+    );
+
+    handleManagerAdded(managerAddedEvent3);
+
+    assert.fieldEquals('CommunityEntity', communityAddress[0], 'managers', '2');
+
+    const dayId = managerAddedEvent2.block.timestamp.toI32() / 86400;
+
+    assert.fieldEquals(
+        'CommunityDailyEntity',
+        `${communityAddress[0]}-${dayId}`,
+        'managers',
+        '2'
+    );
+
+    // assert ubi daily data
+    assert.fieldEquals('UBIDailyEntity', dayId.toString(), 'managers', '2');
+
+    clearStore();
+});
+
 // TODO: test manager on migrated community
