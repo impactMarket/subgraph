6ccb5a6e25ec709c03030197ac65941d321573a4
Bernardo Vieira <bernardo@impactmarket.com>
2022-04-29 09:54:57 +0100

    claim on community entities (#39)

--- generated/schema.ts
+++ generated/schema.ts
@@ -336,6 +336,7 @@ export class CommunityDailyEntity extends Entity {
     this.set("beneficiaries", Value.fromI32(0));
     this.set("managers", Value.fromI32(0));
     this.set("claimed", Value.fromBigDecimal(BigDecimal.zero()));
+    this.set("claims", Value.fromI32(0));
     this.set("contributed", Value.fromBigDecimal(BigDecimal.zero()));
     this.set("contributors", Value.fromI32(0));
     this.set("volume", Value.fromBigDecimal(BigDecimal.zero()));
@@ -416,6 +417,15 @@ export class CommunityDailyEntity extends Entity {
     this.set("claimed", Value.fromBigDecimal(value));
   }
 
+  get claims(): i32 {
+    let value = this.get("claims");
+    return value!.toI32();
+  }
+
+  set claims(value: i32) {
+    this.set("claims", Value.fromI32(value));
+  }
+
   get contributed(): BigDecimal {
     let value = this.get("contributed");
     return value!.toBigDecimal();
@@ -480,6 +490,7 @@ export class UBIEntity extends Entity {
     this.set("beneficiaries", Value.fromI32(0));
     this.set("managers", Value.fromI32(0));
     this.set("claimed", Value.fromBigDecimal(BigDecimal.zero()));
+    this.set("claims", Value.fromI32(0));
     this.set("contributed", Value.fromBigDecimal(BigDecimal.zero()));
     this.set("contributors", Value.fromI32(0));
     this.set("volume", Value.fromBigDecimal(BigDecimal.zero()));
@@ -548,6 +559,15 @@ export class UBIEntity extends Entity {
     this.set("claimed", Value.fromBigDecimal(value));
   }
 
+  get claims(): i32 {
+    let value = this.get("claims");
+    return value!.toI32();
+  }
+
+  set claims(value: i32) {
+    this.set("claims", Value.fromI32(value));
+  }
+
   get contributed(): BigDecimal {
     let value = this.get("contributed");
     return value!.toBigDecimal();
@@ -603,6 +623,7 @@ export class UBIDailyEntity extends Entity {
     this.set("beneficiaries", Value.fromI32(0));
     this.set("managers", Value.fromI32(0));
     this.set("claimed", Value.fromBigDecimal(BigDecimal.zero()));
+    this.set("claims", Value.fromI32(0));
     this.set("contributed", Value.fromBigDecimal(BigDecimal.zero()));
     this.set("contributors", Value.fromI32(0));
     this.set("volume", Value.fromBigDecimal(BigDecimal.zero()));
@@ -670,6 +691,15 @@ export class UBIDailyEntity extends Entity {
 
   set claimed(value: BigDecimal) {
     this.set("claimed", Value.fromBigDecimal(value));
+  }
+
+  get claims(): i32 {
+    let value = this.get("claims");
+    return value!.toI32();
+  }
+
+  set claims(value: i32) {
+    this.set("claims", Value.fromI32(value));
   }
 
   get contributed(): BigDecimal {
