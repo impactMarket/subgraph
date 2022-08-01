edffbe045f2cb4790142f43122b5a570ed5425a1
Bernardo Vieira <bernardo@impactmarket.com>
2022-08-01 20:37:24 +0100

    manager count added removed beneficiaries

--- src/common/beneficiary.ts
+++ src/common/beneficiary.ts
@@ -1,6 +1,6 @@
 import { Address, BigDecimal, BigInt } from '@graphprotocol/graph-ts';
 
-import { BeneficiaryEntity, CommunityEntity, UBIEntity, UserActivityEntity } from '../../generated/schema';
+import { BeneficiaryEntity, CommunityEntity, ManagerEntity, UBIEntity, UserActivityEntity } from '../../generated/schema';
 import { fiveCents, normalize } from '../utils';
 import { loadOrCreateCommunityDaily } from './community';
 import { loadOrCreateDailyUbi } from './ubi';
@@ -19,9 +19,12 @@ export function genericHandleBeneficiaryAdded(
         // load beneficiary
         const beneficiaryId = _beneficiary.toHex();
         let beneficiary = BeneficiaryEntity.load(beneficiaryId);
+        const manager = ManagerEntity.load(_by.toHex())!;
         // alpha version of smart contracts had an issue.
         // it allowed to add a user as beneficiary even though it was a beneficiary already.
         let noContractsAlphaIssue = true;
+        // to prevent a alpha version issue.
+        let previouslyRemoved = true;
 
         if (!beneficiary) {
             beneficiary = new BeneficiaryEntity(beneficiaryId);
@@ -33,20 +36,41 @@ export function genericHandleBeneficiaryAdded(
             beneficiary.claims = 0;
             beneficiary.claimed = BigDecimal.zero();
             beneficiary.since = _blockTimestamp.toI32();
+            beneficiary.addedBy = _by;
+            // update manager
+            manager.added += 1;
         } else if (beneficiary && Address.fromString(beneficiary.community).notEqual(_community)) {
             // save previous entry of beneficiary in another community
             const previousBeneficiary = new BeneficiaryEntity(_hash);
 
+            // if beneficiary added in another community while being active on current one
+            previouslyRemoved = beneficiary.state === 1;
+            if (beneficiary.state === 0) {
+                const previousManager = ManagerEntity.load(beneficiary.addedBy.toHex())!;
+                const previousCommunity = CommunityEntity.load(beneficiary.community)!;
+                
+                // increase removed on manager
+                previousManager.removed += 1;
+                previousManager.save();
+
+                // decrease number of beneficiaries in community
+                previousCommunity.beneficiaries -= 1;
+                previousCommunity.save();
+            }
+
             previousBeneficiary.address = beneficiary.address;
             previousBeneficiary.community = beneficiary.community;
-            previousBeneficiary.state = beneficiary.state;
+            // needs to be as removed
+            previousBeneficiary.state = 1;
             previousBeneficiary.lastClaimAt = beneficiary.lastClaimAt;
             previousBeneficiary.preLastClaimAt = beneficiary.preLastClaimAt;
             previousBeneficiary.claims = beneficiary.claims;
             previousBeneficiary.claimed = beneficiary.claimed;
             previousBeneficiary.since = beneficiary.since;
+            previousBeneficiary.addedBy = beneficiary.addedBy;
+            previousBeneficiary.removedBy = beneficiary.removedBy;
             previousBeneficiary.save();
-            //
+            // update beneficiary
             beneficiary.community = community.id;
             beneficiary.state = 0;
             beneficiary.lastClaimAt = 0;
@@ -54,6 +78,10 @@ export function genericHandleBeneficiaryAdded(
             beneficiary.claims = 0;
             beneficiary.claimed = BigDecimal.zero();
             beneficiary.since = _blockTimestamp.toI32();
+            beneficiary.addedBy = _by;
+            beneficiary.removedBy = null;
+            // update manager
+            manager.added += 1;
         } else if (
             // readded into same community
             beneficiary &&
@@ -63,6 +91,12 @@ export function genericHandleBeneficiaryAdded(
                 noContractsAlphaIssue = false;
             } else {
                 community.removedBeneficiaries -= 1;
+                // update manager if not the same
+                if (beneficiary.addedBy.notEqual(manager.address)) {
+                    manager.added += 1;
+                }
+                // update beneficiary
+                beneficiary.addedBy = _by;
             }
         }
 
@@ -75,30 +109,34 @@ export function genericHandleBeneficiaryAdded(
             activity.community = community.id;
             activity.timestamp = _blockTimestamp.toI32();
             activity.activity = 'ADDED';
-            activity.save();
-            beneficiary.save();
             // update ubi
             const ubi = UBIEntity.load('0')!;
-
-            ubi.beneficiaries += 1;
-            ubi.claimed = ubi.claimed.plus(fiveCents);
-            ubi.save();
-            // update daily ubi
             const ubiDaily = loadOrCreateDailyUbi(_blockTimestamp);
 
-            ubiDaily.beneficiaries += 1;
+            if (previouslyRemoved) {
+                ubi.beneficiaries += 1;
+                ubiDaily.beneficiaries += 1;
+                community.beneficiaries += 1;
+                communityDaily.beneficiaries += 1;
+            }
+
+            ubi.claimed = ubi.claimed.plus(fiveCents);
+            // update daily ubi
             ubiDaily.claimed = ubiDaily.claimed.plus(fiveCents);
-            ubiDaily.save();
             // update community
-            community.beneficiaries += 1;
             community.claimed = community.claimed.plus(fiveCents);
             community.estimatedFunds = community.estimatedFunds.minus(fiveCents);
             community.maxClaim = community.maxClaim.minus(community.decreaseStep);
-            community.save();
             // update community daily
-            communityDaily.beneficiaries += 1;
             communityDaily.claimed = communityDaily.claimed.plus(fiveCents);
+            // save
+            activity.save();
+            beneficiary.save();
+            ubi.save();
+            ubiDaily.save();
+            community.save();
             communityDaily.save();
+            manager.save();
         }
     }
 }
@@ -115,39 +153,45 @@ export function genericHandleBeneficiaryRemoved(
 
     if (community) {
         const beneficiary = BeneficiaryEntity.load(_beneficiary.toHex());
+        const manager = ManagerEntity.load(_by.toHex())!;
 
+        // validate current state (prevent issue)
         if (beneficiary && beneficiary.state !== 1) {
             const communityDaily = loadOrCreateCommunityDaily(_community, _blockTimestamp);
-            // update ubi
             const ubi = UBIEntity.load('0')!;
-
-            ubi.beneficiaries -= 1;
-            ubi.save();
-            // update daily ubi
             const ubiDaily = loadOrCreateDailyUbi(_blockTimestamp);
-
-            ubiDaily.beneficiaries -= 1;
-            ubiDaily.save();
-            // add beneficiary activity
             const activity = new UserActivityEntity(_hash);
-
+            
+            // update ubi
+            ubi.beneficiaries -= 1;
+            // update daily ubi
+            ubiDaily.beneficiaries -= 1;
+            // add beneficiary activity
             activity.user = _beneficiary;
             activity.by = _by;
             activity.community = community.id;
             activity.timestamp = _blockTimestamp.toI32();
             activity.activity = 'REMOVED';
-            activity.save();
             // update beneficiary
             beneficiary.state = 1;
-            beneficiary.save();
+            beneficiary.removedBy = _by;
             // update community
             community.beneficiaries -= 1;
             community.removedBeneficiaries += 1;
             community.maxClaim = community.maxClaim.plus(community.decreaseStep);
-            community.save();
             // update community daily
             communityDaily.beneficiaries -= 1;
+            // update manager
+            manager.removed += 1;
+
+            // save
+            ubi.save();
+            ubiDaily.save();
+            activity.save();
+            beneficiary.save();
+            community.save();
             communityDaily.save();
+            manager.save();
         }
     }
 }
